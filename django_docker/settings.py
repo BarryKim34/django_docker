8b9b51164f2a2e197527ef5d0f8edb8a7d1c7f99
BarryKim34 <dogsooree8@gmail.com>
2026-04-28 09:52:05 +0900

    sec

--- django_docker/settings.py
+++ django_docker/settings.py
@@ -29,7 +29,10 @@ SECRET_KEY = os.getenv('DFANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'localhost',
+    '3.30.35.209'
+]
 
 
 # Application definition
